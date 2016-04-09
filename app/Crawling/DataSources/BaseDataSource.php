42bf2588331c620b6c5913f2fbf930e6fc9b8801
Mathijs Bernson <mathijs.bernson@gmail.com>
2016-04-09 13:44:53 +0200

    Final correction

--- app/Crawling/DataSources/BaseDataSource.php
+++ app/Crawling/DataSources/BaseDataSource.php
@@ -4,8 +4,17 @@ use Log;
 
 abstract class BaseDataSource
 {
+    protected static $common_words = [
+        'voordeel',
+        'vers'
+    ];
+
     protected function guessBrand(string $name): string {
         $brand = explode(' ', $name)[0];
+
+        if(in_array(strtolower($brand), static::$common_words)) {
+            return config('boodschappen.companies')[$this->getCompanyId()];
+        }
 
         return $brand;
     }
